f1224cd54c8808f90e8332723dfdb5416db8c0f2
David Horsley <david.horsley@nviinc.com>
2016-11-10 21:52:27 +0000

    fieldsystem: added sems and strings

--- plugins/inputs/fieldsystem/fieldsystem.go
+++ plugins/inputs/fieldsystem/fieldsystem.go
@@ -3,13 +3,20 @@ package fieldsystem
 import (
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
+	"strings"
 )
 
 type FieldSystem struct {
+	FullRecord bool
+
+	fs         *Fscom
+	prevFields map[string]interface{}
 }
 
 var FieldSystemConfig = `
-  ##Stuff
+  ## Record all fields each gather period, rather
+  ## than just differences
+  #full_record = false 
 `
 
 func (s *FieldSystem) SampleConfig() string {
@@ -17,23 +24,46 @@ func (s *FieldSystem) SampleConfig() string {
 }
 
 func (s *FieldSystem) Description() string {
-	return ""
+	return "Query the Field system state"
 }
 
-func (s *FieldSystem) Gather(acc telegraf.Accumulator) error {
+func (s *FieldSystem) Gather(acc telegraf.Accumulator) (err error) {
+	if s.fs == nil {
+		s.fs, err = GetSHM()
+		if err != nil {
+			return err
+		}
+	}
+
 	fields := make(map[string]interface{})
 	tags := make(map[string]string)
 
-	fs, err := GetFSSHM()
-	if err != nil {
-		return err
+	// Ignore if FS not running
+	if l, err := s.fs.SemLocked("fs"); !l || err != nil {
+		fields["fs"] = false
+		return nil
 	}
-	fmt.Printf("%s\n", fs.LLOG[:])
-	for i := 0; i < int(fs.Sem.Allocated); i++ {
-		v := fs.Sem.Name[i]
-		i, _ := fs.SemLocked(string(v[:]))
-		fmt.Printf("%s, %t\n", v[:], i)
+
+	// FS semephores
+	for i := 0; i < int(s.fs.Sem.Allocated); i++ {
+		semname := strings.TrimSpace(string(s.fs.Sem.Name[i][:]))
+		semval, err := s.fs.SemLocked(semname)
+		if err != nil {
+			continue
+		}
+		fields[semname] = semval
 	}
+
+	// FS bools
+	fields["data_valid"] = (s.fs.Data_valid[0].User_dv != 0)
+	fields["tracking"] = (s.fs.Ionsor != 0)
+
+	// FS strings
+	fields["log"] = string(s.fs.LLOG[:])
+	fields["schedule"] = string(s.fs.LSKD[:])
+	fields["source"] = string(s.fs.Lsorna[:])
+
+	// TODO: diff
 
 	acc.AddFields("fs", fields, tags)
 	return nil
---
+++ plugins/inputs/fieldsystem/utils.go
@@ -0,0 +1,12 @@
+package fieldsystem
+
+// Return the new or updated values in "new"
+func mapdiff(old map[string]interface{}, new map[string]interface{}) map[string]interface{} {
+	diff := make(map[string]interface{})
+	for k, nval := range new {
+		if oval, ok := old["foo"]; !ok || oval != nval {
+			diff[k] = nval
+		}
+	}
+	return diff
+}
