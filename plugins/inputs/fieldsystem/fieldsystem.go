9fa206cb438175349968cfce4ac1c848fc9769d3
David Horsley <david.horsley@nviinc.com>
2017-02-03 10:35:38 -0500

    fieldsystem: converted to new library

--- plugins/inputs/fieldsystem/fieldsystem.go
+++ plugins/inputs/fieldsystem/fieldsystem.go
@@ -1,14 +1,16 @@
 package fieldsystem
 
 import (
+	"fmt"
 	"log"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/internal"
 	"github.com/influxdata/telegraf/plugins/inputs"
+
+	"fs"
 )
 
 type FieldSystem struct {
@@ -16,22 +18,22 @@ type FieldSystem struct {
 	Precision internal.Duration
 
 	sync.Mutex
-	wg   sync.WaitGroup
 	done chan struct{}
+	wg   sync.WaitGroup
 
 	acc telegraf.Accumulator
 
-	fs *Fscom
+	fs fs.FieldSystem
 }
 
 var FieldSystemConfig = `
-  ## Poll the Field System state through shared memory. 
-  ##
-  ## Record RDBE Tsys and Pcal calculated by FS.
-  ## This complements the rdbe_multicast input.
-  #rdbe = false 
-  ## Rate to poll FS variables.
-  #precision = "100ms"
+## Poll the Field System state through shared memory. 
+##
+## Record RDBE Tsys and Pcal calculated by FS.
+## This complements the rdbe_multicast input.
+#rdbe = false 
+## Rate to poll FS variables.
+#precision = "100ms"
 `
 
 func (s *FieldSystem) SampleConfig() string {
@@ -42,6 +44,8 @@ func (s *FieldSystem) Description() string {
 	return "Poll the Field System state through shared memory."
 }
 
+const fsMeas = "fs"
+
 func (s *FieldSystem) Start(acc telegraf.Accumulator) (err error) {
 	s.Lock()
 	defer s.Unlock()
@@ -49,53 +53,176 @@ func (s *FieldSystem) Start(acc telegraf.Accumulator) (err error) {
 	s.done = make(chan struct{})
 
 	s.acc = acc
-	if s.Precision.Duration == 0 {
-		s.Precision.Duration, _ = time.ParseDuration("100ms")
-	}
 	s.acc.SetPrecision(0, s.Precision.Duration)
 
-	s.fs, err = GetSHM()
+	s.fs, err = fs.NewFieldSystem()
 	if err != nil {
 		return err
 	}
 
 	// FS semephores
-	for i := 0; i < int(s.fs.Sem.Allocated); i++ {
-		semname := strings.TrimSpace(string(s.fs.Sem.Name[i][:]))
-		s.wg.Add(1)
-		go s.watchsem(semname)
+
+	for _, semname := range s.fs.Semaphores() {
+		s.watch(fsMeas, semname, func() (interface{}, error) { return s.fs.SemLocked(semname) })
 	}
 
 	// FS bools
-	fsbools := map[string]*int32{
-		"data_valid": &s.fs.Data_valid[0].User_dv,
-		"tracking":   &s.fs.Ionsor,
-	}
-	for name, b := range fsbools {
-		s.wg.Add(1)
-		go s.watchbool(name, b)
-	}
+	s.watch(fsMeas, "data_valid", func() (interface{}, error) { return s.fs.DataValid(), nil })
+	s.watch(fsMeas, "tracking", func() (interface{}, error) { return s.fs.Tracking(), nil })
 
 	// FS strings
-	fsstrings := map[string][]byte{
-		"log":      s.fs.LLOG[:],
-		"schedule": s.fs.LSKD[:],
-		"source":   s.fs.Lsorna[:],
-	}
-	for name, str := range fsstrings {
-		s.wg.Add(1)
-		go s.watchstring(name, str)
-	}
+	s.watch(fsMeas, "log", func() (interface{}, error) { return s.fs.Log(), nil })
+	s.watch(fsMeas, "schedule", func() (interface{}, error) { return s.fs.Schedule(), nil })
+	s.watch(fsMeas, "source", func() (interface{}, error) { return s.fs.Source(), nil })
 
-	if s.Rdbe {
-		for i, _ := range s.fs.Rdbe_tsys_data {
-			s.wg.Add(1)
-			go s.watchrdbe(i)
+	if r, ok := s.fs.(fs.Rdbe); ok {
+		if s.Rdbe {
+			for i := 0; i < r.RdbeNum(); i++ {
+				s.watchrdbe(i)
+			}
 		}
 	}
 
 	log.Println("I! Started FS listener service...")
 	return nil
+}
+
+func (s *FieldSystem) watch(meas string, name string, get func() (interface{}, error)) {
+	s.wg.Add(1)
+	go func() {
+		var oldval interface{}
+		fields := map[string]interface{}{}
+		defer s.wg.Done()
+		for {
+			select {
+			case <-s.done:
+				return
+			case <-time.After(s.Precision.Duration):
+				val, err := get()
+				if err != nil {
+					log.Println(err)
+				}
+				if val != oldval {
+					fields[name] = val
+					s.acc.AddFields(meas, fields, nil, time.Now())
+					oldval = val
+				}
+			}
+		}
+	}()
+}
+
+const (
+	rdbeMeas          = "fs_rdbe"
+	tsysRdbeMeas      = "fs_rdbe_tsys"
+	pcalPhaseRdbeMeas = "fs_rdbe_pcal_phase"
+	pcalAmpRdbeMeas   = "fs_rdbe_pcal_amp"
+)
+
+func (s *FieldSystem) watchrdbe(rdbeindex int) {
+	rdbeid := string('a' + rdbeindex)
+
+	r := s.fs.(fs.Rdbe)
+
+	updated, err := r.RdbeUpdatedFn(rdbeindex)
+	if err != nil {
+		panic(err)
+	}
+
+	s.wg.Add(1)
+	go func() {
+		defer s.wg.Done()
+		for {
+			select {
+			case <-s.done:
+				return
+			case <-time.After(s.Precision.Duration):
+				if !updated() {
+					continue
+				}
+
+				fields := map[string]interface{}{}
+
+				tags := map[string]string{
+					"rdbe": rdbeid,
+				}
+
+				rdbedata, err := r.RdbeMap(rdbeindex)
+				if err != nil {
+					log.Println("E! ", err)
+				}
+
+				now := time.Now()
+
+				fields["epoch"] = rdbedata["Epoch"]
+				fields["epoch_vdif"] = rdbedata["EpochVdif"]
+				fields["pcaloff"] = rdbedata["Pcaloff"]
+				fields["dot2gps"] = rdbedata["Dot2gps"]
+				fields["sigma"] = rdbedata["Sigma"]
+				//fields["pcal_spacing"] = rdbedata.Pcal_spacing
+
+				// Epoch      [14]byte
+				// pad_cgo_0  [2]byte
+				// Epoch_vdif int32
+				// Tsys       [18][2]float32
+				// Pcal_amp   [1024]float32
+				// Pcal_phase [1024]float32
+				// Pcal_ifx   int32
+				// Sigma      float32
+				// Raw_ifx    int32
+				// Dot2gps    float64
+				// Pcaloff    float64
+
+				// Tsys data
+				// Both IFs are updated every seconds
+				tsys := rdbedata["Tsys"].([][]float32)
+				var tsysfields map[string]interface{}
+				for i := 0; i < len(tsys[0]); i++ {
+					tsysfields = map[string]interface{}{}
+					tags = map[string]string{
+						"rdbe": rdbeid,
+						"if":   fmt.Sprintf("%d", i),
+					}
+					for j := 0; j < len(tsys); j++ {
+						if tsys[j][i] < -8e6 || IsNan32(tsys[j][i]) || IsInf32(tsys[j][i]) {
+							continue
+						}
+						tsysfields[fmt.Sprintf("chan_%04d", j)] = tsys[j][i]
+					}
+					s.acc.AddFields(tsysRdbeMeas, tsysfields, tags, now)
+				}
+
+				// Pcal data
+				// alternates between IF every second
+				pcalfields := map[string]interface{}{}
+				tags = map[string]string{
+					"rdbe": rdbeid,
+					"if":   fmt.Sprintf("%d", rdbedata["PcalIfx"]),
+				}
+
+				pcalamp := rdbedata["PcalAmp"].([]float32)
+				for i, amp := range pcalamp {
+					if amp < -8e6 || IsNan32(amp) || IsInf32(amp) {
+						delete(pcalfields, fmt.Sprintf("chan_%04d", i))
+						continue
+					}
+					pcalfields[fmt.Sprintf("chan_%04d", i)] = amp
+				}
+				s.acc.AddFields(pcalAmpRdbeMeas, pcalfields, tags, now)
+
+				pcalphase := rdbedata["PcalPhase"].([]float32)
+				for i, phase := range pcalphase {
+					if phase < -8e6 || IsNan32(phase) || IsInf32(phase) {
+						delete(pcalfields, fmt.Sprintf("chan_%04d", i))
+						continue
+					}
+					pcalfields[fmt.Sprintf("chan_%04d", i)] = phase
+				}
+				s.acc.AddFields(pcalPhaseRdbeMeas, pcalfields, tags, now)
+
+			}
+		}
+	}()
 }
 
 func (s *FieldSystem) Stop() {
@@ -112,5 +239,8 @@ func (s *FieldSystem) Gather(acc telegraf.Accumulator) (err error) {
 }
 
 func init() {
-	inputs.Add("fieldsystem", func() telegraf.Input { return &FieldSystem{} })
+	inputs.Add("fieldsystem", func() telegraf.Input {
+		p, _ := time.ParseDuration("100ms")
+		return &FieldSystem{Precision: internal.Duration{p}}
+	})
 }
--- plugins/inputs/fieldsystem/gen.go
+++
@@ -1,5 +0,0 @@
-//go:generate bash ./types.sh
-//go:generate bash -c "cgo -godefs types.go | sed -e 's/Pad_cgo/pad_cgo/' -e 's/]int8/]byte/g' | gofmt > types_$GOARCH.go"
-// Note: cgo must be patched to fix Issue #5253 (see patch at https://codereview.appspot.com/122900043)
-
-package fieldsystem
